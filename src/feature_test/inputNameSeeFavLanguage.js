b9924a40d98eac71dea86e1f9789861eb95d5c97
Luigi <l-palermo@hotmail.it>
2019-09-26 14:49:23 +0100

    dev: Add feature test "inputNameSeeFavLanguage"

---
+++ src/feature_test/inputNameSeeFavLanguage.js
@@ -0,0 +1,16 @@
+import '../setupTest.js'
+
+describe('F.P.L', () => {
+  it('The user input a user\'s name and see the user favourite language', () => {
+    const browser = await puppeteer.launch({});
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('#user-name');
+    await page.type('#user-name', 'test');
+    await page.click('#submit-name');
+    await page.waitForSelector('#favourite-langauge');
+    const found = await page.evaluate(() => window.find('javascript'))
+    expect(found).toBe(true);
+    await browser.close();
+  })
+})
---
+++ src/testHelper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, render, mount } from "enzyme";
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+const puppeteer = require('puppeteer');
+
+configure({ adapter: new Adapter() });
+
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.puppeteer = puppeteer;
