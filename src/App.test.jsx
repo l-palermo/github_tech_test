12a35ed49604919d3c2391d5b50cbe54021fd125
Luigi <l-palermo@hotmail.it>
2019-10-18 16:06:27 +0100

    refactor: app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './testHelper';
+import UserProgrammingLanguage from './components/UserProgrammingLanguage/UserProgrammingLanguage';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the favourite language main component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(UserProgrammingLanguage)).toHaveLength(1);
 });
